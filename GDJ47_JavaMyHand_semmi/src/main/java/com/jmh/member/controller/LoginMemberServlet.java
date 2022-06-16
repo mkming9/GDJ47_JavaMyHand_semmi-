b0396b1b2c820f777c0106bc356289b5def59033
RyuWonHee <dnjs1505@gmail.com>
2022-06-16 13:50:35 +0900

    getmember생성 login만드는중 member_sql.properties생성

---
+++ GDJ47_JavaMyHand_semmi/src/main/java/com/jmh/member/controller/LoginMemberServlet.java
@@ -0,0 +1,49 @@
+package com.jmh.member.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.jmh.member.model.service.MemberService;
+import com.jmh.member.model.vo.Member;
+
+/**
+ * Servlet implementation class LoginMemberServlet
+ */
+@WebServlet("/member/login.do")
+public class LoginMemberServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public LoginMemberServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String memberId = request.getParameter("memberId");
+		String password = request.getParameter("password");
+//		System.out.println( password);)
+	
+		Member m = new MemberService().loginMember(memberId, password);
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
--- GDJ47_JavaMyHand_semmi/src/main/java/com/jmh/member/model/service/MemberService.java
+++ GDJ47_JavaMyHand_semmi/src/main/java/com/jmh/member/model/service/MemberService.java
@@ -1,5 +1,21 @@
 package com.jmh.member.model.service;
 
+import static com.jmh.common.JDBCTemplate.close;
+import static com.jmh.common.JDBCTemplate.getConnection;
+
+import java.sql.Connection;
+
+import com.jmh.member.model.dao.MemberDao;
+import com.jmh.member.model.vo.Member;
 public class MemberService {
+	
+	private MemberDao dao = new MemberDao();
+	
+	public Member loginMember(String memberId, String password) {
+		Connection conn = getConnection();
+		Member m = dao.loginMember(conn, memberId, password);
+		close(conn);
+		return m;
+	}
 
 }

9cc3e03798f04e5b5ad837b2a6d6363f92f30e8e
epike <epike@BOOK-68LALPDDBA>
2022-06-21 14:20:10 +0900

    220621 선생님 중간 컨펌 포인트업데이트 추가

--- GDJ47_JavaMyHand_semmi/src/main/java/com/jmh/groups/controller/groupsPaymentServlet.java
+++ GDJ47_JavaMyHand_semmi/src/main/java/com/jmh/groups/controller/groupsPaymentServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.jmh.member.model.service.MemberService;
+import com.jmh.member.model.vo.Member;
 
 /**
  * Servlet implementation class groupsPaymentServlet
@@ -27,12 +31,25 @@ public class groupsPaymentServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		HttpSession session = request.getSession();
+
 		request.setCharacterEncoding("UTF-8");
 		String impUid=request.getParameter("imp_uid");
 		String merchantUid=request.getParameter("merchant_uid");
 		String chargeAmount=request.getParameter("charge_amount");
 		
 		System.out.println(impUid+" "+merchantUid+" "+chargeAmount);
+		
+		String saveId = (String) session.getAttribute("saveId");
+		Member m = new MemberService().memberIdCheck(saveId);
+		
+		System.out.println(m);
+		
+		int updatePoint = m.getPoint() + Integer.parseInt(chargeAmount);
+		
+		System.out.println(updatePoint);
+		
+		// int result=new MemberService().UpdateMemberPoint(saveId, updatePoint);
 	}
 
 	/**
